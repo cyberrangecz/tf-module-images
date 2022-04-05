8915bda1c156a424b3ee3428f330dc2a69876585
Tomas Sapak <139890@muni.cz>
2022-04-05 16:21:49 +0200

    Initial commit

---
+++ images.tf
@@ -0,0 +1,21 @@
+resource "openstack_images_image_v2" "focal" {
+  name             = "ubuntu-focal-x86_64"
+  image_source_url = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
+  container_format = "bare"
+  disk_format      = "qcow2"
+
+  properties = {
+    os_type = "linux"
+  }
+}
+
+resource "openstack_images_image_v2" "debian" {
+  name             = "debian-9-x86_64"
+  image_source_url = "http://cdimage.debian.org/cdimage/openstack/current-9/debian-9-openstack-amd64.qcow2"
+  container_format = "bare"
+  disk_format      = "qcow2"
+
+  properties = {
+    os_type = "linux"
+  }
+}
